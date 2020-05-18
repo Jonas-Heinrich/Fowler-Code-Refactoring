2351a5f3b35942c8ca8e60742526c9da69b78a22
Jonas Heinrich <jonas@jonasheinrich.com>
2020-05-18 18:33:46 +0200

    Change spelling in comment

--- src/Program.java
+++ src/Program.java
@@ -4,7 +4,7 @@ import java.util.*;
 /**
  * Note that Java console applications need to be run through the java runtime
  * by running "java -jar JarFile.jar" in the command line.
- * Java console applications can not be previewed in the Compilr IDE, only applets can.
+ * Java console applications can not be previewed in the Compiler IDE, only applets can.
  */
 public class Program {
     /**
